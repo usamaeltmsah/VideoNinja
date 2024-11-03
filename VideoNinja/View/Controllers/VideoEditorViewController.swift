d17f3c59f33a92198841e4ea3c56417e73ab57f4
Usama Fouad <usamaeltmsah@gmail.com>
2024-11-03 21:21:43 +0200

    Create VideoEditorViewController.swift

---
+++ VideoNinja/View/Controllers/VideoEditorViewController.swift
@@ -0,0 +1,13 @@
+//
+//  VideoEditorViewController.swift
+//  VideoNinja
+//
+//  Created by Usama Fouad on 03/11/2024.
+//
+
+import UIKit
+import AVFoundation
+import CoreImage
+import Photos
+
+class VideoEditorViewController: UIViewController {
