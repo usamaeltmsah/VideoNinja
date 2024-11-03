76dd4d3d1ab9fc6803e5aaf4e5b4fc7b2324a250
Usama Fouad <usamaeltmsah@gmail.com>
2024-11-03 21:24:24 +0200

    feat: add outlets and actions for VideoEditorViewController

--- VideoNinja/View/Controllers/VideoEditorViewController.swift
+++ VideoNinja/View/Controllers/VideoEditorViewController.swift
@@ -11,3 +11,32 @@ import CoreImage
 import Photos
 
 class VideoEditorViewController: UIViewController {
+    // Views for trimming controls
+    @IBOutlet weak var mainVideoContainerView: UIView!
+    @IBOutlet weak var trimView: UIView!
+    @IBOutlet weak var startHandleContainer: UIView!
+    @IBOutlet weak var endHandleContainer: UIView!
+    @IBOutlet weak var selectedRangeViewContainer: UIView!
+    
+    // Filter selection buttons
+    @IBOutlet weak var grayscaleButton: UIButton!
+    @IBOutlet weak var sepiaButton: UIButton!
+    // Reset filters button
+    @IBOutlet weak var resetButton: UIButton!
+    
+    // Preview controls
+    @IBOutlet weak var previewContainer: UIView!
+    @IBOutlet weak var videoPlayPauseButton: UIButton!
+    @IBOutlet weak var videoPreviewPlayPauseButton: UIButton!
+    @IBAction func playPauseMainVideo(_ sender: Any) {
+    }
+    @IBAction func togglePreviewPlayback(_ sender: Any) {
+    }
+    @IBAction func applyGrayscaleFilterAction(_ sender: Any) {
+    }
+    @IBAction func applySepiaFilterAction(_ sender: Any) {
+    }
+    @IBAction func resetFiltersAction(_ sender: Any) {
+    }
+    @IBAction func saveAction(_ sender: Any) {
+    }
