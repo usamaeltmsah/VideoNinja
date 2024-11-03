7ef7467c98ff38d31cf2c134dfc1a1269bb3770e
Usama Fouad <usamaeltmsah@gmail.com>
2024-11-03 11:26:56 +0200

    feat: bind view model properties to UI components in VideosListViewController

--- VideoNinja/View/Controllers/VideosListViewController.swift
+++ VideoNinja/View/Controllers/VideosListViewController.swift
@@ -7,16 +7,21 @@
 
 import UIKit
 import Photos
+import Combine
 
 class VideosListViewController: UIViewController {
     @IBOutlet weak var videoCollectionView: UICollectionView!
     @IBOutlet weak var placeholderLabel: UILabel!
+    
+    private var viewModel = VideoEditorViewModel()
+    private var cancellables = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.configureVideoCollectionView()
         self.setupVideoCollectionViewLayout()
+        self.bindViewModel()
     }
     
     // Configures the video collection view by registering the cell, setting the delegate and data source, and initially hiding it.
@@ -37,4 +42,39 @@ class VideosListViewController: UIViewController {
         videoCollectionView.collectionViewLayout = layout
     }
     
+    // Binds the view model properties to the UI components, updating the UI in response to changes.
+    private func bindViewModel() {
+        // Observe the hasPermission property and update the visibility of placeholder and collection view accordingly
+        viewModel.$hasPermission
+            .sink { [weak self] hasPermission in
+                guard let self else { return }
+                self.placeholderLabel.isHidden = hasPermission
+                self.videoCollectionView.isHidden = !hasPermission
+            }
+            .store(in: &cancellables)
+        
+        // Observe the permissionMessage property and update the placeholder label text when it changes
+        viewModel.$permissionMessage
+            .sink { [weak self] message in
+                guard let self else { return }
+                self.placeholderLabel.text = message
+            }
+            .store(in: &cancellables)
+        
+        // Observe the videoAssets property and reload the collection view when the assets change
+        viewModel.$videoAssets
+            .sink { [weak self] videos in
+                guard let self else { return }
+                self.videoCollectionView.reloadData()
+                
+                self.placeholderLabel.isHidden = !videos.isEmpty
+                self.videoCollectionView.isHidden = videos.isEmpty
+                
+                if videos.isEmpty {
+                    placeholderLabel.text = "No selected videos."
+                }
+            }
+            .store(in: &cancellables)
+    }
+}
 }
